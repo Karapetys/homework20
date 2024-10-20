4e5809d6ff7e23a638ac0932c54ad207160c5b9a
Karapetys <142937165+Karapetys@users.noreply.github.com>
2024-10-20 18:59:56 +0400

    Delete lesson 76 directory

--- lesson 76/main.js
+++
@@ -1,7 +0,0 @@
-let name = 'artyom'
-let age = 23
-const sex = 'male'
-let jobStatus = 'unemployed'
-let relationshipStatus = 'single'
-
-console.log(name, age, 'y.o', sex, jobStatus, relationshipStatus)
